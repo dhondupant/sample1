63169d18c18dcd6df59261ceba059f3132a75521
Dell <Dell@DESKTOP-54KFCAF>
2023-08-26 10:34:02 -0700

    First commit

---
+++ src/test/java/demo/sample001.java
@@ -0,0 +1,27 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class sample001
+{
+
+	
+	@Test
+	public void TC1()
+	{
+		Reporter.log("sdfasdfsd");
+	}
+	
+	
+	@Test
+	public void TC2()
+	{
+		Reporter.log("test is executed");
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/demo/sample002.java
@@ -0,0 +1,41 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class sample002 
+{
+
+	
+
+	@Test
+	public void TC3()
+	{
+		Reporter.log("sdfassd");
+	}
+	
+	
+	@Test
+	public void TC4()
+	{
+		Reporter.log("test executed");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
