a3ffcb70468a0fafaea3d6708d624d397b62c385
Dell <Dell@DESKTOP-54KFCAF>
2023-08-26 20:24:53 -0700

    comiiting test to branch xxx

---
+++ src/test/java/demo/sample3.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class sample3 
+{
+
+	
+	
+	@Test
+	public void TC3()
+	{
+		Reporter.log("sdfassd");
+	}
+	
+	
+}
